703a74e45c35c950a65a8cc0f4c0e6e92cefa2d5
pegasus4me <safoantouil@outlook.fr>
2023-05-30 22:12:27 +0200

    fix

--- src/components/pricefeed.jsx
+++ src/components/pricefeed.jsx
@@ -12,7 +12,7 @@ const Pricefeed = () => {
         <iframe
             width="700"
             height="800"
-            className=""
+            className="max-w-[90%]"
             src="https://coinbrain.com/coins/bnb-0x4c9c4a814ef97acfef97dd1b0ebd83712c3a7ed1/lite?theme=custom&accent=ffdc7a&padding=4&background=000000&currency=USD"
           ></iframe>
         </div>
@@ -20,6 +20,7 @@ const Pricefeed = () => {
             <iframe
               width="400"
               height="590"
+              className="max-w-[90%]"
               src="https://coinbrain.com/embed/trade?theme=custom&accent=ffdc7a&padding=4&background=00000020&chainId=56&inputAddress=0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE&outputAddress=0x4c9c4a814EF97AcfEF97dD1B0EbD83712c3A7eD1&outputLocked=1"
             ></iframe>
           </div>
